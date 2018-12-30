14b49b3bbc612814999c64deb02db4c23a3ccbd0
zozospider <2642220896@qq.com>
2018-12-31 00:31:12 +0800

    init

--- src/main/java/com/zozospider/jute/MockReqHeader.java
+++ src/main/java/com/zozospider/jute/MockReqHeader.java
@@ -1,4 +1,19 @@
 package com.zozospider.jute;
 
-public class MockReqHeader {
+import org.apache.jute.InputArchive;
+import org.apache.jute.OutputArchive;
+import org.apache.jute.Record;
+
+import java.io.IOException;
+
+public class MockReqHeader implements Record {
+    @Override
+    public void serialize(OutputArchive archive, String tag) throws IOException {
+
+    }
+
+    @Override
+    public void deserialize(InputArchive archive, String tag) throws IOException {
+
+    }
 }
