8e68a3b9d59a908ac91387e50ef6b923c3f06ebb
zozospider <2642220896@qq.com>
2019-01-01 15:01:35 +0800

    init

--- src/main/java/com/zozospider/jute/MockReqHeader.java
+++ src/main/java/com/zozospider/jute/MockReqHeader.java
@@ -7,13 +7,48 @@ import org.apache.jute.Record;
 import java.io.IOException;
 
 public class MockReqHeader implements Record {
+
+    private long sessionId;
+    private String type;
+
+    public MockReqHeader() {
+    }
+
+    public MockReqHeader(long sessionId, String type) {
+        this.sessionId = sessionId;
+        this.type = type;
+    }
+
+    public long getSessionId() {
+        return sessionId;
+    }
+
+    public void setSessionId(long sessionId) {
+        this.sessionId = sessionId;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
     @Override
     public void serialize(OutputArchive archive, String tag) throws IOException {
-
+        archive.startRecord(this, tag);
+        archive.writeLong(sessionId, "sessionId");
+        archive.writeString(type, "type");
+        archive.endRecord(this, tag);
     }
 
     @Override
     public void deserialize(InputArchive archive, String tag) throws IOException {
-
+        archive.startRecord(tag);
+        sessionId = archive.readLong("sessionId");
+        type = archive.readString("type");
+        archive.endRecord(tag);
     }
+
 }
